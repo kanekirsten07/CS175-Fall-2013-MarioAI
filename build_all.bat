d9ce9fb5be5757836b23a97dcf9b102aba21029d
BrianMacIntosh <bmacbrian@pacbell.net>
2013-11-14 14:19:29 -0800

    Added bats for build, play

---
+++ build_all.bat
@@ -0,0 +1,3 @@
+dir /s /B *.java > build.dat
+javac -cp lib/asm-all-3.3.jar;lib/jdom.jar;lib/junit-4.8.2.jar;lib/testng.jar -d bin/MarioAI @build.dat
+pause
---
+++ play.bat
@@ -0,0 +1,2 @@
+java -cp bin/MarioAI;lib/asm-all-3.3.jar ch.idsia.scenarios.Play
+pause
