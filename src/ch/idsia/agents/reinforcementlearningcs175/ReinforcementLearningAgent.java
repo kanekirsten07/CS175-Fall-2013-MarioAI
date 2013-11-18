71094942d1802a66a545501802b0a0a915de4d6e
Kirsten Kane <kanekirsten07@gmail.com>
2013-11-18 15:53:04 -0800

    Adding Learning Agent Files

--- src/ch/idsia/agents/reinforcementlearningcs175/ReinforcementLearningAgent.java
+++ src/ch/idsia/agents/reinforcementlearningcs175/ReinforcementLearningAgent.java
@@ -1,10 +1,12 @@
 package ch.idsia.agents.reinforcementlearningcs175;
 import ch.idsia.agents.Agent;
+import ch.idsia.agents.LearningAgent;
 import ch.idsia.agents.controllers.BasicMarioAIAgent;
 import ch.idsia.benchmark.mario.engine.sprites.Mario;
 import ch.idsia.benchmark.mario.environments.Environment;
+import ch.idsia.benchmark.tasks.LearningTask;
 
-public class ReinforcementLearningAgent implements Agent{
+public class ReinforcementLearningAgent implements Agent, LearningAgent{
 
 	private String name;
 	
@@ -47,6 +49,55 @@ public class ReinforcementLearningAgent implements Agent{
 
 	@Override
 	public void reset() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void learn() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void giveReward(float reward) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void newEpisode() {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void setLearningTask(LearningTask learningTask) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public void setEvaluationQuota(long num) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	@Override
+	public Agent getBestAgent() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+
+	@Override
+	public void init() {
 		// TODO Auto-generated method stub
 		
 	}
