b2e7876d6859f3f3bf950ff49d68755f478881ba
kschong <kenneth.s.chong@gmail.com>
2013-11-26 14:04:04 -0800

    Changed weights to be stored to a 1 dimensional array for each agent. You can now select to get a percentage of the top scoring agents for use in the next generation.

--- src/ch/idsia/agents/reinforcementlearningcs175/GeneticManager.java
+++ src/ch/idsia/agents/reinforcementlearningcs175/GeneticManager.java
@@ -2,17 +2,23 @@ package ch.idsia.agents.reinforcementlearningcs175;
 import java.util.ArrayList;
 
 public class GeneticManager 
-{
+{	
 	private ArrayList<GeneticManagerAgent> agents;
 	private int currentAgentCount;
 	
-	public GeneticManager()
+	/*
+	 * Select top x percentage of agents to generate the next generation.
+	 */
+	private float topPercentageToSave;
+	
+	public GeneticManager(float topPercentageToSave)
 	{
 		currentAgentCount = 0;
 		agents = new ArrayList<GeneticManagerAgent>();
+		this.topPercentageToSave = topPercentageToSave;
 	}
 	
-	public void saveAgent(float[][][] weights, float score)
+	public void saveAgent(float[] weights, float score)
 	{
 		agents.add(new GeneticManagerAgent(currentAgentCount, weights, score));
 		++currentAgentCount;
@@ -26,7 +32,7 @@ public class GeneticManager
 			return null;
 	}
 	
-	public float[][][] getAgentWeights(int agentID)
+	public float[] getAgentWeights(int agentID)
 	{
 		GeneticManagerAgent agent = getAgent(agentID);
 		if(agent != null)		
@@ -56,5 +62,30 @@ public class GeneticManager
 		}
 		
 		return toReturn;
+	}
+	
+	public ArrayList<GeneticManagerAgent> getTopAgents()
+	{
+		ArrayList<GeneticManagerAgent> topAgents = new ArrayList<GeneticManagerAgent>();
+		int numAgents = (int)(agents.size() * topPercentageToSave);
+		
+		while(topAgents.size() < numAgents && !agents.isEmpty())
+		{
+			GeneticManagerAgent topAgent = getHighestScoringAgent();
+			topAgents.add(topAgent);
+			agents.remove(topAgent.getAgentID());			
+		}
+		
+		return topAgents;
+	}
+	
+	public float getTopPercentageToSave()
+	{
+		return topPercentageToSave;
+	}
+
+	public void setTopPercentageToSave(float topPercentageToSave) 
+	{
+		this.topPercentageToSave = topPercentageToSave;
 	}
 }
--- src/ch/idsia/agents/reinforcementlearningcs175/GeneticManagerAgent.java
+++ src/ch/idsia/agents/reinforcementlearningcs175/GeneticManagerAgent.java
@@ -3,17 +3,17 @@ package ch.idsia.agents.reinforcementlearningcs175;
 public class GeneticManagerAgent 
 {
 	private int 		agentID;
-	private float[][][] agentWeights;
+	private float[]		agentWeights;
 	private float		agentScore;
 	
-	public GeneticManagerAgent(int agentID, float[][][] weights, float score)
+	public GeneticManagerAgent(int agentID, float[] weights, float score)
 	{
 		this.agentID = agentID;
 		this.agentWeights = weights;
 		this.agentScore = score;
 	}
 
-	public float[][][] getAgentWeights() 
+	public float[] getAgentWeights() 
 	{
 		return agentWeights;
 	}
