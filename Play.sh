fecf83ad648dae0430f08f98b14852326ee365c1
Lawrence Yu <lawlyu@gmail.com>
2013-12-03 13:03:36 -0800

    Modified Play.sh to pass on command line arguments to the Java program

--- Play.sh
+++ Play.sh
@@ -1,2 +1,5 @@
 cd bin/
-java -cp ../lib/asm-all-3.3.jar:. ch.idsia.scenarios.Main
+java -cp ../lib/asm-all-3.3.jar:. ch.idsia.scenarios.Main $@
+
+# Example usage (use our agent, and use level seed of 5)
+# ./Play.sh -ag ch.idsia.agents.reinforcementlearningcs175.ReinforcementLearningAgent -ls 5
